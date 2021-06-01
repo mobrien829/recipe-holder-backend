371c5b2b0eb0bb878e0b9399c1c13ecb6b260156
Michael <mobrien@colgate.edu>
2021-06-01 11:31:27 -0400

    wrote index.html

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// express and DB requires
+const express = require("express");
+require("./db/mongoose")
+
+// routers
+
+// const userRouter = require("./routers/user")
+// const taskRouter = require("./routers/task")
+
+// connect the app to everything else
+const app = express();
+const port = process.env.port
+
+// assign express functions to app
+
+app.use(express.json());
+// app.use(userRouter);
+// app.use(taskRouter);
+
+app.listen(port, () => {
+    console.log(`server on ${port}`)
+})
